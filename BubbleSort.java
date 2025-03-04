eaeebb1bfc0353db5173ecccbd0cffbac7d1e448
Arka Mondal <arka@pitangent.com>
2025-03-04 18:30:23 +0530

    bubble sort updated

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,20 +1,25 @@
-public class BubbleSort { //  check two adjacent elements every time
+public class BubbleSort { // check two adjacent elements every time
     public static void main(String[] args) {
-        int[] arr = { 20,30,20,10,40 };
+        int[] arr = { 10,20, 30, 40, 50 };
+        boolean swapped = false;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;    // if array is sorted then it will go to j loop once to check whether array is sorted or not. then is swapped=false break out
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length-1;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                    
-                    for(int val:arr) System.out.print(val+", ");
+                    for (int val : arr)
+                        System.out.print(val + ", ");
                     System.out.println();
                 }
             }
+            if(!swapped) break;
         }
         System.out.println("Sorted Array");
-        for(int val:arr) System.out.print(val+", ");
+        for (int val : arr)
+            System.out.print(val + ", ");
     }
 }

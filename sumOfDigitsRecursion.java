beac59e9151112b0d2df0459ce8ce364e9ddc767
Arka Mondal <arka@pitangent.com>
2025-03-18 18:40:33 +0530

    sum of didgits using recursion added

---
+++ sumOfDigitsRecursion.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class sumOfDigitsRecursion {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number");
+        int n = Integer.parseInt(br.readLine());
+        System.out.println("Sum is "+sum(n));
+    }
+
+    static int sum(int n){
+        if(n==0) return 0;
+        int q= n/10;
+        int r=n-q*10;
+        return r+sum(q);
+    }
+}

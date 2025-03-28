70eb03038b7e9c4388b3aab7f464e134a3369a83
arka mondal <arka@pitangent.com>
2025-03-28 18:05:23 +0530

    selectionSort recursion nedds to be done

---
+++ selectionSortRecursion.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class selectionSortRecursion {
+    public static void main(String[] args) {
+        int[] arr = { 16, 14, 11, 5, 4, 3, 2, 1, 9, 8, 7, 10 };
+        System.out.println("Sorted Array " + Arrays.toString(selectionSort(arr, 0, 0)));
+    }
+
+    static int[] selectionSort(int[] arr, int fIndex, int sIndex) {
+        return arr;
+
+    }
+}

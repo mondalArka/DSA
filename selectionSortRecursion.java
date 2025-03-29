86979153854528d12f2cc187237a241d496b50c4
mondalArka <arka6740@gmail.com>
2025-03-29 11:50:15 +0530

    Add: selection sort recursion done

--- selectionSortRecursion.java
+++ selectionSortRecursion.java
@@ -3,11 +3,22 @@ import java.util.Arrays;
 public class selectionSortRecursion {
     public static void main(String[] args) {
         int[] arr = { 16, 14, 11, 5, 4, 3, 2, 1, 9, 8, 7, 10 };
-        System.out.println("Sorted Array " + Arrays.toString(selectionSort(arr, 0, 0)));
+        System.out.println("Sorted Array " + Arrays.toString(selectionSort(arr, 0, 1)));
     }
 
     static int[] selectionSort(int[] arr, int fIndex, int sIndex) {
-        return arr;
+       
+        if(fIndex>arr.length-1) return arr;
+        if (sIndex > arr.length - 1)
+            return selectionSort(arr, fIndex + 1, fIndex+1+1);
+        if (arr[fIndex] > arr[sIndex]) {
+            System.out.println(fIndex+" "+sIndex);
+            int temp = arr[sIndex];
+            arr[sIndex] = arr[fIndex];
+            arr[fIndex] = temp;
+            System.out.println(Arrays.toString(arr));
+        }
+        return selectionSort(arr, fIndex, sIndex + 1);
 
     }
 }

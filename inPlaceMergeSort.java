8f38730104f16e0ef88abfdac6f94fb1b043fc60
arka mondal <arka@pitangent.com>
2025-03-31 14:21:07 +0530

    add:Inplace merge sort

---
+++ inPlaceMergeSort.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+public class inPlaceMergeSort {
+    public static void main(String[] args) { // complexity is nlogn and write is 
+        int[] arr={ 5,4,3,2,1,8,9};
+        breakArr(arr, (arr.length-1/2), 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void breakArr(int[] arr,int mid,int s,int e){
+        System.out.println(Arrays.toString(arr));
+        if(s==e) return;
+        mid=(s+e)/2;
+         breakArr(arr,mid,s,mid);
+         breakArr(arr,mid, mid+1, e);
+
+        merge(arr, mid, s, e);  
+       System.out.println(Arrays.toString(arr));
+    }
+
+    static void merge(int[] arr,int mid,int s,int e){
+        int[] arr2= new int[(e-s)+1];
+        int k=0;
+        int i=s;
+        int j=mid+1;
+        while(i<=mid && j<=e){
+            if(arr[i]>arr[j]){
+                arr2[k]=arr[j];
+                k++;
+                j++;
+            }else {
+                arr2[k]=arr[i];
+                k++;
+                i++;
+            }
+        }
+        // System.out.println("i "+i+" j "+j);
+
+      
+            while(i<=mid){
+                // System.out.println("i "+i+" j "+j);
+                arr2[k]=arr[i];
+                k++;
+                i++;
+            }
+        
+       
+            while (j<=e) {
+                arr2[k]=arr[j];
+                k++;
+                j++;
+            }
+        
+        for(int l=0;l<arr2.length;l++){
+            arr[s+l]=arr2[l];
+           
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}

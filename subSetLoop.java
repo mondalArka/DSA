1c21259594914cf0545ec50d4dca36934e3cd954
arka mondal <arka@pitangent.com>
2025-04-10 15:58:39 +0530

    add: recursion subsets no repeat add and loop style also

--- subSetLoop.java
+++ subSetLoop.java
@@ -3,18 +3,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class subSetLoop { // vvimp
-    public static void main(String[] args) {
-        int[] arr = { 3, 1, 2 };
-        System.out.println(subSet(arr));
-    //    System.out.println(subSet2("abc"));
+    public static void main(String[] args) { // spacae and time complexity is always 2^n*n
+        int[] arr = { 3, 1, 1,6 };
+        // System.out.println(subSet(arr)); // for simple  array of integers
+        // System.out.println(subSet2("abc")); // for characters subsets String input
+        System.out.println(noRepeatSubSet(arr)); // no repeat subsets
     }
 
     static List<List<Integer>> subSet(int[] arr) { // always 2^n*n
-        List<List<Integer>> outer= new ArrayList<>();
+        List<List<Integer>> outer = new ArrayList<>(); // always taking each list of outerlist and inserting a specific value to it and then inserting it to outer to get the combos
         outer.add(new ArrayList<>());
-        for (int num:arr){
-            int n= outer.size();
-            for(int i=0;i<n;i++){
+        for (int num : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
                 List<Integer> inner = new ArrayList<>(outer.get(i));
                 inner.add(num);
                 outer.add(inner);
@@ -25,20 +26,42 @@ public class subSetLoop { // vvimp
     }
 
     static List<List<Character>> subSet2(String s) {
-        List<List<Character>> outer= new ArrayList<>();
+        List<List<Character>> outer = new ArrayList<>();
         outer.add(new ArrayList<>());
-        char[] arr= s.toCharArray();
-        System.out.println(Arrays.toString(arr)+" char arr");
+        char[] arr = s.toCharArray();
+        System.out.println(Arrays.toString(arr) + " char arr");
 
-        for(char c:arr){
-            int n =outer.size();
-            for(int i=0;i<n;i++){
+        for (char c : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
                 List<Character> inner = new ArrayList<>(outer.get(i));
                 inner.add(c);
                 outer.add(inner);
             }
         }
         outer.removeFirst();
+        return outer;
+    }
+
+    static List<List<Integer>> noRepeatSubSet(int[] arr) { // repeats can only be removed if they are adjacent and to achieve that use sorting like insertion or merge anything so that the repeated elements can be side by side.
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            if (i > 0 && arr[i - 1] == arr[i])
+                start = end + 1; // end here is the last iteration loop value not the current outer length
+
+            end = outer.size() - 1; 
+            int n = outer.size();
+            for (int j = start; j < n; j++) {
+                List<Integer> inner = new ArrayList<>(outer.get(j));
+                inner.add(arr[i]);
+                outer.add(inner);
+            }
+        }
+        // outer.removeFirst();
         return outer;
     }
 }
--- subSetRecursionNumbers.java
+++ subSetRecursionNumbers.java
@@ -9,20 +9,23 @@ public class subSetRecursionNumbers { // complexity 2^n*n and space is also same
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // System.out.println("Enter the number");
-        int[] n = { 3, 1, 2,4 };
-        List<List<Integer>> list= subSet(n, new ArrayList<>(1), 0, new ArrayList<>((int)Math.pow(2, n.length)-1));
-       System.out.println(Arrays.toString(list.toArray())); 
+        int[] n = { 3, 2, 2,6 };
+        List<List<Integer>> list = subSet(n, new ArrayList<>(1), 0, new ArrayList<>((int) Math.pow(2, n.length) - 1));
+        System.out.println(Arrays.toString(list.toArray()));
     }
 
     static List<List<Integer>> subSet(int[] actualArr, List<Integer> n, int s, List<List<Integer>> arr) {
+        if (s > actualArr.length - 1) { // non repetative done
+            List<Integer> copy = new ArrayList<>(n);
+            if (arr.contains(copy))
+                return arr;
 
-        if (s > actualArr.length - 1) {
             arr.add(new ArrayList<>(n));
             return arr;
         }
-        n.add(actualArr[s]);
+        n.add(actualArr[s]); // n is filled with current value of s and the array is inserted to outer list 
         subSet(actualArr, n, s + 1, arr);
-        n.removeLast();
+        n.removeLast(); // that actualArr[s] value is removed so that on the right side it can eneter with not selected approach
         subSet(actualArr, n, s + 1, arr);
 
         return arr;

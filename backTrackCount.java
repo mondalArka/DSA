db1786618561b2c73c691f188a5d55d0e0a76330
arka mondal <arka@pitangent.com>
2025-04-23 16:01:28 +0530

    add: print paths in a matrix

--- backTrackCount.java
+++ backTrackCount.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class backTrackCount {
     static int count = 0;
 
@@ -6,7 +8,12 @@ public class backTrackCount {
 
         System.out.println(paths(arr.length, count, 0, 0) + " number of paths"); // number of paths
 
-        printPaths(arr.length, "", 0, 0);
+        // printPaths(arr.length, "", 0, 0);
+
+        System.out.println("the paths are ");
+        ArrayList<ArrayList<ArrayList<Integer>>> path = printPaths(arr.length, new ArrayList<>(), new ArrayList<>(), 0, 0);
+
+        path.forEach(x -> System.out.println(x));
     }
 
     static int paths(int l, int cp, int r, int c) {
@@ -18,23 +25,33 @@ public class backTrackCount {
         else if (c >= l - 1) // when it reaches the bottom end then only go right
             return paths(l, cp, r + 1, c);
 
-        return paths(l, cp, r, c + 1) + paths(l, cp, r + 1, c); // left one is for going right and right side is for going down
+        return paths(l, cp, r, c + 1) + paths(l, cp, r + 1, c); // left one is for going right and right side is for
+                                                                // going down
     }
 
-    static void printPaths(int l, String p, int r, int c) {
-        if (r >= l - 1 && c >= l - 1) {
-            System.out.println(p+" is the path");
-            return;
+    static ArrayList<ArrayList<ArrayList<Integer>>> printPaths(int l, ArrayList<ArrayList<ArrayList<Integer>>> arr, ArrayList<ArrayList> newArr, int r, int c) {
+        if (r >= l-1 && c >= l-1) {
+            // System.out.println(p+" is the path");
+            ArrayList<Integer> innerArr = new ArrayList<>();
+            innerArr.add(r);
+            innerArr.add(c);
+            newArr.add(new ArrayList(innerArr));
+            arr.add(new ArrayList(newArr));
+            newArr.remove(newArr.size() - 1);
+            return arr;
         }
-        if (r >= l - 1) {
-            printPaths(l, p, r, c + 1);
-            return;
-        } else if (c >= l - 1) {
-            printPaths(l, p, r + 1, c);
-            return;
+        if (r >= l) {
+            return arr;
+        } else if (c >= l) {
+            return arr;
         }
-
-        printPaths(l, p,r + 1, c);
-        printPaths(l, p, r, c + 1);
+        ArrayList<Integer> innerArr = new ArrayList<>();
+        innerArr.add(r);
+        innerArr.add(c);
+        newArr.add(new ArrayList(innerArr));
+        printPaths(l, arr, newArr, r + 1, c);
+        printPaths(l, arr, newArr, r, c + 1);
+        newArr.remove(newArr.size() - 1);
+        return arr;
     }
 }

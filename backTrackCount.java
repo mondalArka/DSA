fe2ec745251d934fb830473b0f3b9080e2039455
arka mondal <arka@pitangent.com>
2025-04-24 18:37:05 +0530

    add: diagonal backTrack code

--- backTrackCount.java
+++ backTrackCount.java
@@ -6,14 +6,21 @@ public class backTrackCount {
     public static void main(String[] args) {
         int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
-        System.out.println(paths(arr.length, count, 0, 0) + " number of paths"); // number of paths
+        // System.out.println(paths(arr.length, count, 0, 0) + " number of paths"); //
+        // number of paths
 
         // printPaths(arr.length, "", 0, 0);
 
-        System.out.println("the paths are ");
-        ArrayList<ArrayList<ArrayList<Integer>>> path = printPaths(arr.length, new ArrayList<>(), new ArrayList<>(), 0, 0);
+        // System.out.println("the paths are ");
+        // ArrayList<ArrayList<ArrayList<Integer>>> path = printPaths(arr.length, new
+        // ArrayList<>(), new ArrayList<>(), 0,
+        // 0);
 
-        path.forEach(x -> System.out.println(x));
+        // path.forEach(x -> System.out.println(x));
+        // -----------------------------------------------------------
+
+        // print Directions
+        printDirection(arr.length, "", 0, 0);
     }
 
     static int paths(int l, int cp, int r, int c) {
@@ -29,8 +36,9 @@ public class backTrackCount {
                                                                 // going down
     }
 
-    static ArrayList<ArrayList<ArrayList<Integer>>> printPaths(int l, ArrayList<ArrayList<ArrayList<Integer>>> arr, ArrayList<ArrayList> newArr, int r, int c) {
-        if (r >= l-1 && c >= l-1) {
+    static ArrayList<ArrayList<ArrayList<Integer>>> printPaths(int l, ArrayList<ArrayList<ArrayList<Integer>>> arr,
+            ArrayList<ArrayList> newArr, int r, int c) {
+        if (r >= l - 1 && c >= l - 1) {
             // System.out.println(p+" is the path");
             ArrayList<Integer> innerArr = new ArrayList<>();
             innerArr.add(r);
@@ -53,5 +61,25 @@ public class backTrackCount {
         printPaths(l, arr, newArr, r, c + 1);
         newArr.remove(newArr.size() - 1);
         return arr;
+    }
+
+    static void printDirection(int l, String p, int r, int c) {
+        if (r >= l - 1 && c >= l - 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r >= l - 1) {
+            printDirection(l, p + "R", r, c + 1);
+            return;
+        } else if (c >= l - 1) {
+            printDirection(l, p + "D", r + 1, c);
+            return;
+        }
+        printDirection(l, p + "D", r + 1, c); // for downside
+        printDirection(l, p + "R", r, c + 1); // for right
+    }
+
+    static void diagonalCount(int l , int cp,int r, int c){
+        
     }
 }
